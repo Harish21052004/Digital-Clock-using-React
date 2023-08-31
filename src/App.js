112a034be98d7e2aec8c2cad96128fa762558630
Harish21052004 <vinayaga7643@gmail.com>
2023-08-31 22:20:38 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date())
+    }, 1000);
+    return () => clearInterval(interval)
+  }, [])
+
+  const hrs = time.getHours() > 12 ? time.getHours() - 12 : time.getHours()
+  const min = time.getMinutes()<10 ?'0' +time.getMinutes(): time.getMinutes()
+  const sec = time.getSeconds() < 10 ?  '0' +time.getSeconds(): time.getSeconds()
+  const states = time.getHours() < 13 ? 'AM' : 'PM'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='cover'>
+      <div class="container">
+        <div class="wrapper">
+          <div class="clock">
+            <div class="hours" id="hours">{hrs}</div>
+            <label for="hours">Hours [{states}]</label>
+          </div>
+          :
+          <div class="clock">
+            <div class="minutes" id="minutes">{min}</div>
+            <label for="minutes">Minutes</label>
+          </div>
+          :
+          <div class="clock">
+            <div class="seconds" id="seconds">{sec}</div>
+            <label for="seconds">Seconds</label>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
